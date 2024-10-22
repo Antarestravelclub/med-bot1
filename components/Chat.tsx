ddba9a99412dd3c8f5db6a868c38d14bb2b58757
Antares Travel Club <mpiffmx@yahoo.com>
2024-10-22 18:38:42 -0500

    Update Chat.tsx

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,52 +1,20 @@
-"use client";
+import { getHumeAccessToken } from "@/utils/getHumeAccessToken";
+import dynamic from "next/dynamic";
 
-import { VoiceProvider } from "@humeai/voice-react";
-import Messages from "./Messages";
-import Controls from "./Controls";
-import StartCall from "./StartCall";
-import { ComponentRef, useRef } from "react";
+const Chat = dynamic(() => import("@/components/Chat"), {
+  ssr: false,
+});
 
-export default function ClientComponent({
-  accessToken,
-}: {
-  accessToken: string;
-}) {
-  const timeout = useRef<number | null>(null);
-  const ref = useRef<ComponentRef<typeof Messages> | null>(null);
+export default async function Page() {
+  const accessToken = await getHumeAccessToken();
 
-  // optional: use configId from environment variable
-  const configId = process.env['NEXT_PUBLIC_HUME_CONFIG_ID'];
-  
+  if (!accessToken) {
+    throw new Error();
+  }
+
   return (
-    <div
-      className={
-        "relative grow flex flex-col mx-auto w-full overflow-hidden h-[0px]"
-      }
-    >
-      <VoiceProvider
-        auth={{ type: "accessToken", value: accessToken }}
-        configId={configId}
-        onMessage={() => {
-          if (timeout.current) {
-            window.clearTimeout(timeout.current);
-          }
-
-          timeout.current = window.setTimeout(() => {
-            if (ref.current) {
-              const scrollHeight = ref.current.scrollHeight;
-
-              ref.current.scrollTo({
-                top: scrollHeight,
-                behavior: "smooth",
-              });
-            }
-          }, 200);
-        }}
-      >
-        <Messages ref={ref} />
-        <Controls />
-        <StartCall />
-      </VoiceProvider>
+    <div className={"grow flex flex-col"}>
+      <Chat accessToken={accessToken} />
     </div>
   );
 }
